b359013a08168a0ffbca2b17b43cccaa3cdb6858
Hubert Stärk <hubert.staerk@gmail.com>
2025-05-28 21:21:34 +0200

    better output when starting the api

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { youtrackRouter } from './routes/youtrack';
 import { errorHandler } from './middleware/errorHandler';
+import pico from 'picocolors'
 
 // Create Express app
 const app = express();
@@ -21,7 +22,11 @@ app.use(errorHandler);
 // Function to start the server
 export const startServer = (port: number | string) => {
   return app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`YouTrack Api running on port ${port}`);
+    console.log();
+    console.log('Examples:');
+    console.log(`  - curl http://localhost:${port}/api/health`);
+    console.log(`  - curl http://localhost:${port}/api/ticket/PROJECT-123`);
   });
 };
 
